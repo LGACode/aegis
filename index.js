9a31be872b2409e45acb44d9f46f0798c4641008
Jeremy Miller <jeremy@dischan.org>
2016-04-10 12:45:11 -0400

    Verify github signature before processing webhook.

--- index.js
+++ index.js
@@ -2,9 +2,28 @@ var Discord = require("discord.js");
 var config = require('./config');
 var express = require('express');
 var bodyParser = require('body-parser');
+var crypto = require('crypto');
 var app = express();
 
 var aegis = new Discord.Client();
+
+function verifyGithubSignature(key, data, githubSig) {
+  var sig = 'sha1=' + crypto.createHmac('sha1', key).update(data).digest('hex');
+  return sig === githubSig;
+}
+
+function rawBody(req, res, next) {
+  req.rawBody = '';
+  req.setEncoding('utf8');
+
+  req.on('data', function(chunk) {
+    req.rawBody += chunk;
+  });
+
+  req.on('end', function() {
+    next();
+  });
+}
 
 aegis.on('message', function(message){
   if(message.content === 'smug')
@@ -16,25 +35,30 @@ aegis.on("ready", function () {
 });
 
 aegis.login(config.discord.email, config.discord.password).then(function(){
+  app.use(rawBody);
   app.use(bodyParser.json());
 
   app.post('/github/webhook', function (req, res) {
     var event = req.get('X-GitHub-Event');
-    var data = req.body;
-    switch(event){
-      case 'push':
-      var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commit${data.commits.length > 1 ? s : ''} to ${data.repository.name} ( ${data.repository.html_url} )!`;
-      message += '\n\n```';
-      for (var i = 0; i < data.commits.length; i++) {
-        message += `\n\n${data.commits[i].author.name}\n${data.commits[i].id}\n${data.commits[i].message}`;
+    var signature = req.get('X-Hub-Signature');
+    if(!verifyGithubSignature(config.github.secret, req.rawBody, signature)){
+      res.status(401).send('Invalid signature.');
+    } else {
+      var data = req.body;
+      switch(event){
+        case 'push':
+        var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commit${data.commits.length > 1 ? s : ''} to ${data.repository.name} ( ${data.repository.html_url} )!`;
+        message += '\n\n```';
+        for (var i = 0; i < data.commits.length; i++) {
+          message += `\n\n${data.commits[i].author.name}\n${data.commits[i].id}\n${data.commits[i].message}`;
+        }
+        message += '\n```';
+        aegis.sendMessage(aegis.channels.get("name", "general"), message);
+        break;
       }
-      message += '\n```';
-      aegis.sendMessage(aegis.channels.get("name", "general"), message);
-      break;
+
+      res.send('Success');
     }
-
-
-    res.send('Success');
   });
 
   app.listen(8008, function () {
