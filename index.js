0f79a810267dc27be5b51c078629a3cb93baf794
Jeremy Miller <jeremy@dischan.org>
2016-04-09 22:43:04 -0400

    Remove commit link since previews in Discord are excessive. Remove some log statements.

--- index.js
+++ index.js
@@ -12,25 +12,21 @@ aegis.on('message', function(message){
 });
 
 aegis.on("ready", function () {
-  console.log(aegis.channels.get("name", "general"));
   console.log("Ready to begin! Serving in " + aegis.channels.length + " channels");
 });
 
 aegis.login(config.discord.email, config.discord.password).then(function(){
-  console.log(aegis.channels.getAll("name", "general"));
   app.use(bodyParser.json());
 
   app.post('/github/webhook', function (req, res) {
     var event = req.get('X-GitHub-Event');
     var data = req.body;
-    console.log(data);
-    console.log(event);
     switch(event){
       case 'push':
       var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commits to ${data.repository.name} ( ${data.repository.html_url} )!`;
       message += '\n\n```';
       for (var i = 0; i < data.commits.length; i++) {
-        message += `\n\n${data.commits[i].author.name}\n${data.commits[i].id}\n${data.commits[i].message}\n${data.commits[i].url}`;
+        message += `\n\n${data.commits[i].author.name}\n${data.commits[i].id}\n${data.commits[i].message}`;
       }
       message += '\n```';
       aegis.sendMessage(aegis.channels.get("name", "general"), message);
