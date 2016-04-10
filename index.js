2b14c6714af4984dffaf6ec920fcbcecbaed9f05
Jeremy Miller <jeremy@dischan.org>
2016-04-09 21:45:10 -0400

    Add body parser. Parse github webhooks and hopefully send message to dischan chat.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Discord = require("discord.js");
 var config = require('./config');
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-
 
 var mybot = new Discord.Client();
 
@@ -12,14 +12,30 @@ mybot.on('message', function(message){
 });
 
 mybot.login(config.discord.email, config.discord.password).then(function(){
-  console.log(mybot.channels);
-});
+  app.use(bodyParser.json());
 
-app.post('/github/webhook', function (req, res) {
-  console.log(req.body)
-  res.send('Success');
-});
+  app.post('/github/webhook', function (req, res) {
+    var event = req.get('X-GitHub-Event');
+    var data = req.body;
+    console.log(data);
+    console.log(event);
+    switch(event){
+      case 'push':
+      var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commits to ${data.repository.name} (${data.repository.html_url})!`;
+      message += '\n```';
+      for (var i = 0; i < data.commits.length; i++) {
+        message += `\n${data.commits[i].author.name} - ${data.commits[i].id} - ${data.commits[i].message} - ${data.commits[i].url}`;
+      }
+      message += '\n```';
+      mybot.sendMessage('general', message);
+      break;
+    }
 
-app.listen(8008, function () {
-  console.log('Aegis is listening on port 8008');
+
+    res.send('Success');
+  });
+
+  app.listen(8008, function () {
+    console.log('Aegis is listening on port 8008');
+  });
 });
