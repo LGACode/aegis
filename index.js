aca1efa1064510bc949c96f474a79f774d6446ea
Jeremy Miller <jeremy@dischan.org>
2016-04-09 22:31:56 -0400

    Change push notification format.

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ aegis.login(config.discord.email, config.discord.password).then(function(){
     console.log(event);
     switch(event){
       case 'push':
-      var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commits to ${data.repository.name} (${data.repository.html_url})!`;
-      message += '\n```';
+      var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commits to ${data.repository.name} ( ${data.repository.html_url} )!`;
+      message += '\n\n```';
       for (var i = 0; i < data.commits.length; i++) {
-        message += `\n${data.commits[i].author.name} - ${data.commits[i].id} - ${data.commits[i].message} - ${data.commits[i].url}`;
+        message += `\n\n${data.commits[i].author.name}\n${data.commits[i].id}\n${data.commits[i].message}\n${data.commits[i].url}`;
       }
       message += '\n```';
       aegis.sendMessage(aegis.channels.get("name", "general"), message);
