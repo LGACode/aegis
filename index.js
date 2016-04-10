376e1576f5100a99c1723b6967b0af32361e14f4
Jeremy Miller <jeremy@dischan.org>
2016-04-10 12:59:41 -0400

    Do this in a better way and fix bug.

--- index.js
+++ index.js
@@ -12,19 +12,6 @@ function verifyGithubSignature(key, data, githubSig) {
   return sig === githubSig;
 }
 
-function rawBody(req, res, next) {
-  req.rawBody = '';
-  req.setEncoding('utf8');
-
-  req.on('data', function(chunk) {
-    req.rawBody += chunk;
-  });
-
-  req.on('end', function() {
-    next();
-  });
-}
-
 aegis.on('message', function(message){
   if(message.content === 'smug')
     aegis.reply(message, 'snug smug');
@@ -35,19 +22,18 @@ aegis.on("ready", function () {
 });
 
 aegis.login(config.discord.email, config.discord.password).then(function(){
-  app.use(rawBody);
   app.use(bodyParser.json());
 
   app.post('/github/webhook', function (req, res) {
     var event = req.get('X-GitHub-Event');
     var signature = req.get('X-Hub-Signature');
-    if(!verifyGithubSignature(config.github.secret, req.rawBody, signature)){
+    if(!verifyGithubSignature(config.github.secret, JSON.stringify(req.body), signature)){
       res.status(401).send('Invalid signature.');
     } else {
       var data = req.body;
       switch(event){
         case 'push':
-        var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commit${data.commits.length > 1 ? s : ''} to ${data.repository.name} ( ${data.repository.html_url} )!`;
+        var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commit${data.commits.length > 1 ? 's' : ''} to ${data.repository.name} ( ${data.repository.html_url} )!`;
         message += '\n\n```';
         for (var i = 0; i < data.commits.length; i++) {
           message += `\n\n${data.commits[i].author.name}\n${data.commits[i].id}\n${data.commits[i].message}`;
