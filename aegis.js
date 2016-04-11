eec6f938e652e0c07204633e4340455eae255020
Jeremy Miller <jeremy@dischan.org>
2016-04-10 22:13:14 -0400

    Add code to allow Aegis to update Yuki.

--- aegis.js
+++ aegis.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var crypto = require('crypto');
 var version = require('./package.json').version;
 const fs = require('fs');
+var proc = require('child_process');
 var app = express();
 
 var aegis = new Discord.Client();
@@ -42,13 +43,36 @@ aegis.login(config.discord.email, config.discord.password).then(function(){
       var data = req.body;
       switch(event){
         case 'push':
-        var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commit${data.commits.length > 1 ? 's' : ''} to ${data.repository.name} ( ${data.repository.html_url} )!`;
-        message += '\n\n```';
-        for (var i = 0; i < data.commits.length; i++) {
-          message += `\n\n${data.commits[i].author.name}\n${data.commits[i].id}\n${data.commits[i].message}`;
-        }
-        message += '\n```';
-        aegis.sendMessage(aegis.channels.get("name", "general"), message);
+          var message = `Wai~! ${data.pusher.name} just pushed ${data.commits.length} commit${data.commits.length > 1 ? 's' : ''} to ${data.repository.name} ( ${data.repository.html_url} )!`;
+          message += '\n\n```';
+          for (var i = 0; i < data.commits.length; i++) {
+            message += `\n\n${data.commits[i].author.name}\n${data.commits[i].id}\n${data.commits[i].message}`;
+          }
+          message += '\n```';
+          aegis.sendMessage(aegis.channels.get("name", "general"), message, function(){
+            if(data.repository.name==='yuki'){
+              message = `Since ${data.commits.length > 1 ? 'these' : 'this'} update is for Yuki, I'm going to try to update her myself...`;
+              aegis.sendMessage(aegis.channels.get("name", "general"), message, function(){
+                fs.writeFileSync(`${__dirname}/../yuki/update.flag`, 'updating');
+                //update yuki
+                try {
+                  //kill bot
+                  console.log('killing yuki');
+                  proc.execSync('kill -9 $(ps aux | grep \'[y]uki.js\' | awk \'{print $2}\')');
+                } finally {
+                  //pull updates
+                  console.log('pulling updates');
+                  proc.execSync('git pull', {cwd: `${__dirname}/../yuki`});
+                  //start bot
+                  console.log('restarting bot');
+                  proc.execSync('node yuki.js > stdout.txt 2> stderr.txt &', {cwd: `${__dirname}/../yuki`});
+
+                  message = `I updated and restarted her, she's probably fine... (✿ •́ ‸ •̀ )`;
+                  aegis.sendMessage(aegis.channels.get("name", "general"), message);
+                }
+              });
+            }
+          });
         break;
       }
 
@@ -76,6 +100,18 @@ aegis.login(config.discord.email, config.discord.password).then(function(){
           res.send('Success');
         }, 2000);
       });
+    }
+  });
+
+  app.post('/yuki/notify', function (req, res) {
+    var signature = req.get('X-Yuki-Signature');
+    if(!verifySignature(config.yuki.secret, JSON.stringify(req.body), signature)) {
+      res.status(401).send('Invalid signature.');
+    } else {
+      var data = req.body;
+      var message = `She just called, and she's totally fine! Yatta! ٩(✿˃̵ᴗ˂̵)و`;
+
+      aegis.sendMessage(aegis.channels.get("name", "general"), message);
     }
   });
 
